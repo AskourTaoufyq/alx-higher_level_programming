61aa689914166e93373377dfa210d32565ea3d70
AskourTaoufyq <kosa477kacm@gmail.com>
2024-01-19 07:37:09 -0800

    python-data_structures

---
+++ 0x03-python-data_structures/9-max_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+def max_integer(my_list=[]):
+    if not my_list:
+        return 'None'
+    max_v = my_list[0]
+    for i in range(0, len(my_list)):
+        if max_v < my_list[i]:
+            max_v = my_list[i]
+    return max_v
---
+++ 0x03-python-data_structures/README.md
@@ -0,0 +1,1 @@
+readme file of data structures
