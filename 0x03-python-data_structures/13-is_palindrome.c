dc6f48a8e4cbc7fb5f81ae35cbb22dfef2af2736
AskourTaoufyq <kosa477kacm@gmail.com>
2024-01-22 15:18:54 -0800

    data structures

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,41 +1,53 @@
 #include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
 
 /**
- * is_palindrome - checks if a linked list is a palindrome
- * @head: double pointer to the linked list
- * Return: 1 if it is, 0 if not
- */
+*add_nodeint - adds a new node at the beginning of a listint_t list
+*@head: head of listint_t
+*@n: int to add in listint_t list
+*Return: address of the new element, or NULL if it failed
+*/
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new;
+
+	new = malloc(sizeof(listint_t));
+	if (new == NULL)
+		return (NULL);
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
+/**
+*is_palindrome - identify if a syngle linked list is palindrome
+*@head: head of listint_t
+*Return: 1 if it is palindrome else 0
+*/
 int is_palindrome(listint_t **head)
 {
-	int length = 0, i = 0;
-	listint_t *temp = NULL;
+	listint_t *head2 = *head;
+	listint_t *aux = NULL, *aux2 = NULL;
 
-	if (head == NULL)
-		return (0);
-
-	temp = *head;
-	while (temp != NULL)
+	if (*head == NULL || head2->next == NULL)
+		return (1);
+	while (head2 != NULL)
 	{
-		temp = temp->next;
-		length++;
+		add_nodeint(&aux, head2->n);
+		head2 = head2->next;
 	}
-
-	int arr[length];
-
-	temp = *head;
-	while (temp != NULL)
+	aux2 = aux;
+	while (*head != NULL)
 	{
-		arr[--length] = temp->n;
-		temp = temp->next;
-	}
-
-	temp = *head;
-	while (temp != NULL)
-	{
-		if (temp->n != arr[i])
+		if ((*head)->n != aux2->n)
+		{
+			free_listint(aux);
 			return (0);
-		temp = temp->next;
-		i++;
+		}
+		*head = (*head)->next;
+		aux2 = aux2->next;
 	}
+	free_listint(aux);
 	return (1);
 }
